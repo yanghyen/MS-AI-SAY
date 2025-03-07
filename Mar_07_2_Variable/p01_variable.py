5db20fc7566b09ae5599b75c582357d0ad28eb30
yanghyen <sk0310803@naver.com>
2025-03-07 15:38:21 +0900

    docs: Mar 07 TIL 업로드

--- Mar_07_2_Variable/p01_variable.py
+++ Mar_07_2_Variable/p01_variable.py
@@ -1,2 +1,15 @@
+# 정수형 int
 a = 10
-print(a)
+print(a, type(a), id(a))
+
+# 실수형 float
+b = 1.1
+print(b, type(b), id(b))
+
+# 문자열 string
+c = "hi"
+print(c, type(c), id(c))
+
+# 논리형 boolean
+d = True
+print(d, type(d), id(d))
