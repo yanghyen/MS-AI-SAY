b75563bf5104422d11e93b8f1c9270e7894d6639
yanghyen <sk0310803@naver.com>
2025-03-14 15:40:27 +0900

    docs: TIL Mar 14 업로드

---
+++ RepeatStmt/p02_repeatStmt.py
@@ -0,0 +1,23 @@
+# shift 이용
+value = int(input("값을 입력하세용 : "))
+if value >= (1 << 3):
+    print("24시간")
+    value -= (1 << 3)
+if value >= (1 << 2):
+    print("흡연실")
+    value -= (1 << 2)
+if value >= (1 << 1):
+    print("주차장")
+    value -= (1 << 1)
+if value >= (1 << 0):
+    print("와이파이")
+    value -= (1 << 0)
+
+# -----------------------------------
+# shift & 반복문
+# value = int(input("값을 입력하세용 : "))
+value = 13
+for i in range(3, -1):
+    if value >= (1 << i):
+        print("24시간")
+        value -= (1 << i)
--- Summary/RepeatStmt.md
+++ Summary/RepeatStmt.md
@@ -5,3 +5,14 @@
 ### for
 - for 변수 in 컬렉션:  
     내용
+- dictionary에 추천되는 사용법
+    - .items() : 
+    ```py
+    c = {"기온" : 20, "미세먼지" : "심함"}
+    for v in c.items():
+        print(v, type(v))
+    >>>
+    ('기온', 20) <class 'tuple'>
+    ('미세먼지', '심함') <class 'tuple'>
+    ```
+    
