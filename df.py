2949b94fb1916c59d4c0bd24c692dae27425db9a
yanghyen <sk0310803@naver.com>
2025-04-17 13:15:59 +0900

    docs: Apr 17 TIL(html, css) upload

--- df.py
+++ df.py
@@ -1,5 +1,7 @@
-n = 144
-print(n ** (1/2))
-
-n = 976
-print(n ** (1/2))
+answer = []
+num_list = [1, 2]
+print(len(num_list))
+for i in reversed(range(len(num_list))):
+    print(i)
+    answer.append(num_list[i])
+print(answer)
